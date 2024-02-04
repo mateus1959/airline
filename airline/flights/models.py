0d8db2316682e2dfbaf56cb0a9d96e9e8ec9e18e
Mateus Machado <mateus.machado@saintleo.edu>
2024-02-04 14:49:13 -0500

    Add model to flight/models.py

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Flight(models.Model):
+    origin = models.CharField(max_lenght=64)
+    destination = models.CharField(max_lenght=64)
+    duration = models.IntegerField()
