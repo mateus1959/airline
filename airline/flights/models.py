407f5025595e3ae30a75e358b00f969c7b146c91
Mateus Machado <mateus.machado@saintleo.edu>
2024-02-04 14:57:20 -0500

    Make migration

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Flight(models.Model):
-    origin = models.CharField(max_lenght=64)
-    destination = models.CharField(max_lenght=64)
+    origin = models.CharField(max_length=64)
+    destination = models.CharField(max_length=64)
     duration = models.IntegerField()
