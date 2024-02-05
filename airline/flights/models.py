5d66bae2ee7f69cd745b3383b2d2f22a98d283b9
Mateus Machado <mateus.machado@saintleo.edu>
2024-02-05 12:07:33 -0500

    Update flights app

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -8,6 +8,7 @@ class Airport(models.Model):
     def __str__(self):
         return f"{self.city} ({self.code})"
 
+
 class Flight(models.Model):
     origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
     destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
@@ -15,3 +16,12 @@ class Flight(models.Model):
 
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination}"
+    
+
+class Passenger(models.Model):
+    first = models.CharField(max_length=64)
+    last = models.CharField(max_length=64)
+    flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
+
+    def __str__(self) -> str:
+        return f"{self.first} {self.last}"
